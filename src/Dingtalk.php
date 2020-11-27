749c4cf2f27a9a3923c24b154d43ca85f5a4a01c
iscxy <iscxy@hotmail.com>
2020-11-27 09:04:51 +0800

    update

--- src/Dingtalk.php
+++ src/Dingtalk.php
@@ -2,17 +2,39 @@
 
 namespace Iscxy\Dingtalk;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+
 class Dingtalk
 {
     protected $accesstoken;
+    protected $appkey;
+    protected $appsecret;
 
     public function __construct()
     {
         $this->getAccessToken();
     }
 
-    public function getAccessToken()
+    public function getAccessToken($appkey = '')
     {
+        if ($appkey == '') {
+            return json_encode([]);
+        } else {
+            # code...
+        }
+        
+        if (Cache::has('AppKey_'.$appkey)) {
+            //
+        } else {
+            # code...
+        }
+
+        $url = "https://oapi.dingtalk.com/gettoken?appkey=". $this->appkey ."&appsecret=". $this->appsecret;
+
+
+
         return "";
     }
 
