b474ce34ec1cd78b6d4f584aba57aa7185c6376b
iscxy <iscxy@hotmail.com>
2020-11-28 17:24:03 +0800

    update

--- README.cn.md
+++ README.cn.md
@@ -16,6 +16,8 @@
 
 |errCode|errMsg|原因|排查方法|
 |:----|:----|:----|:----|
+|200001|Http请求错误||检查Http请求的URL|
 |210001|缺少AppKey|empty($appkey)为空|检查传入的\$appkey变量值|
+|210001|缺少AppSecret|empty($appsecret)为空|检查传入的\$appsecret变量值|
 |:----|:----|:----|:----|
 |:----|:----|:----|:----|
--- src/Dingtalk.php
+++ src/Dingtalk.php
@@ -9,27 +9,59 @@ use Illuminate\Support\Facades\Http;
 class Dingtalk
 {
     
-    public function getAccessToken($appkey = '')
+    public function getAccessToken($appkey = '',$appsecret='')
     {
-        if (empty($appkey)) {
+        if ( empty($appkey) ) {
             return json_encode([
                 'errCode' => 210001,
                 'errMsg' => '缺少AppKey',
                 ]);
-        } else {
-            if (Cache::has('Dingtalk_AccessToken_'.$appkey)) {
+        } elseif( empty($appsecret) ) {
+            return json_encode([
+                'errCode' => 210002,
+                'errMsg' => '缺少AppSecret',
+                ]);
+        }else {
+            if ( Cache::has('Dingtalk_AccessToken_'.$appkey) ) {
                 return Cache::get('Dingtalk_AccessToken_'.$appkey);
             } else {
-                $a = json_encode([
+                $response = Http::get('https://oapi.dingtalk.com/gettoken?appkey='. $appkey .'&appsecret='. $appsecret)->json();
+                if ($response->successful()) {
+
+                    dd($response);
+                    
+                } else {
+                    return json_encode([
+                        'errCode' => 200001,
+                        'errMsg' => 'Http请求错误',
+                        ]);
+                }
+                
+                /*
+                $accesstoken = json_encode([
                         'access_token'=>'fw8ef8we8f76e6f7s8dxxxx',
                         'errcode'=>'0',
                         'errmsg'=>'ok',
                         'expires_in'=>'7200'
                 ]);
-                Cache::forever('Dingtalk_AccessToken_'.$appkey, $a);
-                return $a;
+                Cache::forever('Dingtalk_AccessToken_'.$appkey, $accesstoken);
+                return $accesstoken;
+
+                */
+
+
             }
         }
+        
+
+
+
+
+
+        //         Cache::forever('Dingtalk_AccessToken_'.$appkey, $a);
+        //         return $a;
+        //     }
+
     }
 
 }
