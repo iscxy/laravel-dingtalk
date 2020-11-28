159cc126c578d105841a74eed8986771b04802fa
iscxy <iscxy@hotmail.com>
2020-11-28 08:47:10 +0800

    update

--- src/Dingtalk.php
+++ src/Dingtalk.php
@@ -2,9 +2,9 @@
 
 namespace Iscxy\Dingtalk;
 
+use Illuminate\Support\Facades\Cache;
 
 
-// use Illuminate\Support\Facades\Cache;
 // use Illuminate\Support\Facades\Http;
 
 
@@ -19,7 +19,11 @@ class Dingtalk
                 'errMsg' => '缺少AppKey',
                 ]);
         } else {
-            return "getAccessToken";
+            if (Cache::has('AccessToken_'.$appkey)) {
+                return Cache::get('AccessToken_'.$appkey);
+            } else {
+                return "get new AccessToken";
+            }
         }
     }
 
