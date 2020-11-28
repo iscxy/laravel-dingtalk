cad9b1cc5aff82f8d168d60630af900a431c63bd
iscxy <iscxy@hotmail.com>
2020-11-28 13:36:40 +0800

    update

--- src/Dingtalk.php
+++ src/Dingtalk.php
@@ -21,10 +21,10 @@ class Dingtalk
                 return Cache::get('Dingtalk_AccessToken_'.$appkey);
             } else {
                 $a = json_encode([
-                        "access_token"=>"fw8ef8we8f76e6f7s8dxxxx",
-                        "errcode"=>"0",
-                        "errmsg"=>"ok",
-                        "expires_in"=>"7200"
+                        'access_token'=>'fw8ef8we8f76e6f7s8dxxxx',
+                        'errcode'=>'0',
+                        'errmsg'=>'ok',
+                        'expires_in'=>'7200'
                 ]);
                 Cache::forever('Dingtalk_AccessToken_'.$appkey, $a);
                 return $a;
