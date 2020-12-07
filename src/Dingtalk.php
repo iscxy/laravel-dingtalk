00ed108947cf1154848fa922bd4411db6452b8cd
iscxy <iscxy@hotmail.com>
2020-12-07 17:35:01 +0800

    update

--- src/Dingtalk.php
+++ src/Dingtalk.php
@@ -4,133 +4,153 @@ namespace Iscxy\Dingtalk;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ConnectException;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+
+
+
+// use Illuminate\Support\Facades\DB;
 
 class Dingtalk
 {
 
-    private $httpClient;
+    protected $httpClient;
+    protected $Monolog;
 
     public function __construct()
     {
         $this->httpClient = new Client([
-            'timeout'  => 5.0,
+            'timeout'  => 10.0,
             'verify' => false,
-            'headers' => [
-                'Accept' => 'application/json',
-            ],
         ]);
+        if ( array_key_exists('logfilename', Config::get('dingtalk')) && !empty(Config::get('dingtalk.logfilename')) ) {
+            $this->Monolog = new Logger('Dingtalk');
+            $this->Monolog->pushHandler(new StreamHandler(storage_path('logs/'.Config::get('dingtalk.logfilename')) , Logger::DEBUG));
+        }
     }
 
     /**
      * 获取AccessToken
      * @param   string  $appkey 钉钉应用的唯一标识key。
-     * @return  object  { "errCode": 0, "errMsg": "ok", "accesstoken": "4d54e90d66793c15ac4ca7e91a29904b", "expires": 1607157220 }
+     * @return  array  ["errCode"=>0, "errMsg"=>"ok", "accesstoken"=>"4d54e90d66793c15ac4ca7e91a29904b", "expires"=>1607157220]
      */
-    public function getAccessToken($appkey = '')
+    public function getAccessToken($appkey)
     {
-        if ( empty($appkey) ) {
-            return json_encode(['errCode' => 210001,'errMsg' => '缺少AppKey',]);
+        if (Cache::has('Dingtalk_AccessToken_'.$appkey)) {
+            $rs = Cache::get('Dingtalk_AccessToken_'.$appkey);
+            if ( $rs['expires'] > time() ) {
+                return $rs;
+            } else {
+                return $this->getRefresh($appkey);
+            }
         } else {
-            if (Cache::has('Dingtalk_AccessToken_'.$appkey)) {
-                $rs = json_decode(Cache::get('Dingtalk_AccessToken_'.$appkey));
-                if ( $rs->expires > time() ) {
-                    return $rs;
-                } else {
-                    return $this->getRefreshAccessToken($appkey);
+            return $this->getRefresh($appkey);
+        }
+    }
+
+    /**
+     * 刷新AccessToken行为
+     * @param   string  $appkey     应用的唯一标识。
+     * @return  array   ["errCode"=>0, "errMsg"=>"ok", "accesstoken"=>"4d54e90d66793c15ac4ca7e91a29904b", "expires"=>1607157220]
+     */
+    public function getRefresh($appkey = '')
+    {
+        $keySecret = $this->getKeySecretList();
+        if ( array_key_exists('errCode', $keySecret) && $keySecret['errCode'] == 0 ) {
+            if (empty($appkey)) {
+                //刷新所有AccessToken
+                foreach ($keySecret['lists'] as $appkey => $appsecret) {
+                    $this->Monolog->ERROR('刷新全部AccessToken');
+                    $this->refreshAccessToken($appkey,$appsecret);
                 }
             } else {
-                return $this->getRefreshAccessToken($appkey);
-            }
-        }
-    }
-
-    /**
-     * 刷新单个AccessToken
-     * @param   string  $appkey 钉钉应用的唯一标识key。
-     * @return  object  { "errCode": 0, "errMsg": "ok", "accesstoken": "4d54e90d66793c15ac4ca7e91a29904b", "expires": 1607157220 }
-     */
-    public function getRefreshAccessToken($appkey = '')
-    {
-        $appsecret = '';
-        //获取 配置中 的 appkey 储存方式
-        $configType = Config::get('dingtalk.type', 'config');
-        switch ($configType) {
-            case 'config':
-                if ( empty($appkey) ) {
-                    return json_encode(['errCode' => 210001,'errMsg' => '缺少AppKey或AppSecret',]);
+                if (array_key_exists($appkey, $keySecret['lists'])) {
+                    return $this->refreshAccessToken($appkey,$keySecret['lists'][$appkey]);
                 } else {
-                    $appkeyList = Config::get('dingtalk.config');
-                    if ( array_key_exists($appkey, $appkeyList) ) {
-                        $appsecret = $appkeyList["$appkey"];
-                    } else {
-                        return json_encode(['errCode' => 210002,'errMsg' => '无该AppKey项对应配置信息',]);
-                    }
+                    return ['errCode' => 210002,'errMsg' => '无该AppKey项对应配置信息',];
                 }
-                break;
-            case 'database':
-                break;
-            default:
-                return json_encode(['errCode' => 210102,'errMsg' => '未知的 appkey 配置方式',]);
-                break;
+            }
+        } else {
+            return $keySecret;
         }
-        $this->refreshAccessToken($appkey,$appsecret);
-    }
-
-    /**
-     * 刷新所有AccessToken
-     * @return  object  { "errCode": 0, "errMsg": "ok", "accesstoken": "4d54e90d66793c15ac4ca7e91a29904b", "expires": 1607157220 }
-     */
-    public function refreshAllAccessToken()
-    {
-
     }
 
     /**
      * 刷新单个AccessToken
      * @param   string  $appkey     应用的唯一标识。
      * @param   string  $appsecret  应用的密钥。
-     * @return  object  { "errCode": 0, "errMsg": "ok", "accesstoken": "4d54e90d66793c15ac4ca7e91a29904b", "expires": 1607157220 }
+     * @return  array  ["errCode"=>0, "errMsg"=>"ok", "accesstoken"=>"4d54e90d66793c15ac4ca7e91a29904b", "expires"=>1607157220]
      */
     protected function refreshAccessToken($appkey,$appsecret)
     {
         if ( empty($appkey) || empty($appsecret) ) {
-            return json_encode(['errCode' => 210001,'errMsg' => '缺少AppKey或AppSecret',]);
+            return ['errCode' => 210001,'errMsg' => '缺少AppKey或AppSecret',];
         } else {
             try {
                 $rs = $this->httpClient->get('https://oapi.dingtalk.com/gettoken?appkey='. $appkey .'&appsecret='. $appsecret);
                 $getAT = json_decode($rs->getBody()->getContents(),true);
-                /* $getAT =json_decode( (string) "{"errcode":0,"access_token":"4d54e90d66793c15ac4ca7e91a29904b","errmsg":"ok","expires_in":7200}"  ,true) */
                 if (  array_key_exists('errcode', $getAT) ) {
                     if ( $getAT['errcode'] == 0 ) {
-                        $accesstoken = json_encode([
+                        $accesstoken = [
                             'errCode' => $getAT['errcode'],
                             'errMsg' => $getAT['errmsg'],
                             'accesstoken' => $getAT['access_token'],
                             'expires' => time() -10 + (int)$getAT['expires_in'],
-                        ]);
+                        ];
                         if ( Cache::forever('Dingtalk_AccessToken_'.$appkey, $accesstoken) ) {
                             return $accesstoken;
                         } else {
-                            return json_encode(['errCode' => 200002,'errMsg' => '将AccessAoken写入Cache缓存失败']);
+                            return ['errCode' => 200002,'errMsg' => '将AccessAoken写入Cache缓存失败'];
                         }
                     } else {
-                        return json_encode(['errCode' => 210004,'errMsg' => '钉钉全局错误：[ '.$getAT['errcode'].' ]'.$getAT['errmsg'],]);
+                        return ['errCode' => 210004,'errMsg' => '钉钉全局错误：[ '.$getAT['errcode'].' ]'.$getAT['errmsg'],];
                     }
                 } else {
-                    return json_encode(['errCode' => 210003,'errMsg' => '返回数据中缺少errcode键名',]);
+                    return ['errCode' => 210003,'errMsg' => '返回数据中缺少errcode键名',];
                 }
             } catch (ConnectException $e) {
-                return json_encode(['errCode' => 200001,'errMsg' => 'Http请求错误:-> '.substr($e->getMessage(),0,strpos($e->getMessage()," (")),]);
+                return ['errCode' => 200001,'errMsg' => 'Http请求错误:-> '.substr($e->getMessage(),0,strpos($e->getMessage()," (")),];
             }
         }
     }
 
-
-
-
-
-
+    /**
+     * 返回{appkey => appsecret}数组列表
+     * @param   string  $appkey     应用的唯一标识。
+     * @return array   ["errCode"=>0, "errMsg"=>"ok", "lists"=>["appkey1"=> "appsecret1","appkey2"=> "appsecret2"]]
+     */
+    protected function getKeySecretList()
+    {
+        //获取 配置中 的 appkey 储存方式
+        $configType = Config::get('dingtalk.type', 'config');
+        switch ($configType) {
+            case 'config':
+                return [
+                    "errCode" => 0,
+                    "errMsg" => "ok",
+                    "lists" => Config::get('dingtalk.config'),
+                ];
+                break;
+            case 'database':
+#region 未完成代码
+                // $this->Monolog->ERROR('缓存中没有这个AppKey对应的AccessToken');
+                return [
+                    "errCode" => 0,
+                    "errMsg" => "ok",
+                    "lists" => Config::get('dingtalk.config'),
+                ];
+                break;
+#endregion               
+            default:
+                return [
+                    'errCode' => 210102,
+                    'errMsg' => '未知的配置方式',
+                ];
+                break;
+        }      
+    }
 }
